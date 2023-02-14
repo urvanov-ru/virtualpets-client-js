ba47d070b967a84d025d17f58e0382947e0c99a3
Fedor Urvanov <fedor@urvanov.ru>
2023-02-14 15:32:15 +0300

    Moved code from myfile.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-//import _ from 'lodash';
+import AchievementInfoGameObject from './domain/AchievementInfoGameObject.js';
+import AddPrivateChatArg from './domain/AddPrivateChatArg.js'
+import AnimationOverArg from './domain/AnimationOverArg.js'
+import AnimationOverListener from './domain/AnimationOverListener.js';
+import BookGameObject from './domain/BookGameObject.js';
+import BuildingGameObject from './domain/BuildingGameObject.js';
+import BuildingMaterialGameObject from './domain/BuildingMaterialGameObject.js';
+import BuildMenuGameObject from './domain/BuildMenuGameObject.js';
+import ClickedArg from './domain/ClickedArg.js';
+import ClickedListener from './domain/ClickedListener.js';
+import ClothGameObject from './domain/ClothGameObject.js';
+import CollectableGameObject from './domain/CollectableGameObject.js';
+import Dimension from './domain/Dimension.js';
+import DressingRoomData from './domain/DressingRoomData.js';
+import EventListener from './domain/EventListener.js';
+import TownData from './domain/TownData.js';
 
-//function component() {
-//  const element = document.createElement('div');
 
-//  // lodash, now imported by this script
-//  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+let o = new AchievementInfoGameObject();
+o.achievementLabel = 'test';
+alert(o.achievementLabel);
 
-//  return element;
-//}
+//let addPrivateChatArg = new AddPrivateChatArg();
 
-//document.body.appendChild(component());
+
