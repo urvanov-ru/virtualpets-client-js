1dcf43d0c694e94dacc1045d13d6900f9c761cb3
Fedor Urvanov <fedor@urvanov.ru>
2023-07-03 18:30:21 +0300

    ProgressBarGameObjectRender

--- src/view/component/ProgressBarGameObjectRender.js
+++ src/view/component/ProgressBarGameObjectRender.js
@@ -1,19 +1,30 @@
 import GameObjectRender from './GameObjectRender.js';
+import Point from '../../domain/Point.js';
 
 
 export default class ProgressBarGameObjectRender extends GameObjectRender {
 
-  #progressBarGameObject;
-    
   constructor(go) {
     super(go);
-    this.#progressBarGameObject = go;
   }
 
   step() {
   }
-
-  get progressBarGameObject() {
-	this.#progressBarGameObject;
+  
+  draw(independentCanvas) {
+    const context = independentCanvas.context;
+    const scale = independentCanvas.scale;
+    const logicPosition = this.gameObject.position;
+    this.position = new Point(logicPosition.x * scale,
+        logicPosition.y * scale);
+    const logicDimension = this.gameObject.dimension;
+    this.dimension = new Dimension(logicDimension.width * scale,
+        logicDimension.height * scale);
+    const lastColor = context.fillStyle;
+    context.setColor("#ffff00");
+    context.drawRect(this.position.x, position.y, dimension.width, dimension.height);
+    const value = this.gameObject.value / this.gameObject.maxValue * dimension.width;
+    context.fillRect(this.position.x, this.position.y, value, this.dimension.height);
+    context.fillStyle(lastColor);
   }
 }
