8a9d9ad024e08acd929c6706ceca51620462aa6b
Fedor Urvanov <fedor@urvanov.ru>
2023-07-07 15:50:38 +0300

    Manifest and resources

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
 
 module.exports = {
   mode: 'development',
   entry: ['./src/index.js'],
   devtool: 'inline-source-map',
   watch: true,
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "manifest.json", to: "manifest.json" },
+        { from: "data", to: "data" },
+        { from: "index.html", to: "index.html" },
+      ],
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
