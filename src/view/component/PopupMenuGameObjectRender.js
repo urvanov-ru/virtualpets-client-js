0e6dbb1c8eb3a36492e4ebfd47df4cd37325c584
Fedor Urvanov <fedor@urvanov.ru>
2023-11-14 18:33:23 +0300

    Popup menu emulation with HTML dialog element

--- src/view/component/PopupMenuGameObjectRender.js
+++ src/view/component/PopupMenuGameObjectRender.js
@@ -1,13 +1,51 @@
 import GameObjectRender from './GameObjectRender.js';
-
+import {mainContainerElement} from '../container.js';
 
 export default class PopupMenuGameObjectRender extends GameObjectRender {
 
   #popupMenuGameObject;
+  
+  #dialogEl;
     
   constructor(go) {
     super(go);
     this.#popupMenuGameObject = go;
+    this.#dialogEl = document.createElement('dialog');
+    
+    const containerDiv = document.createElement('div');
+    containerDiv.style.width = '100%';
+    containerDiv.style.height = '100%';
+    containerDiv.style.display = 'flex';
+    containerDiv.style.flexDirection = 'column';
+    
+    const titleDiv = document.createElement('div');
+    const closeButton = document.createElement('button');
+    const titleText = document.createElement('div');
+    
+    closeButton.innerText = 'X';
+    titleText.innerText = 'title';
+    
+    titleDiv.style.display = 'flex';
+    titleDiv.style.flexFlow = 'row nowrap';
+    closeButton.style.marginLeft = 'auto';
+      
+    titleDiv.append(titleText);
+    titleDiv.append(closeButton);
+    
+    containerDiv.append(titleDiv);
+    
+    this.#dialogEl.append(containerDiv);
+    for (let menuItem of this.#popupMenuGameObject.menuItems) {
+      let button = document.createElement('button');
+      button.innerText = menuItem.text;
+      containerDiv.append(button);
+    }
+    mainContainerElement().append(this.#dialogEl);
+    
+    closeButton.addEventListener('click', (event) => {
+        this.#dialogEl.close();
+        this.#popupMenuGameObject.visible = false;
+    });
   }
 
   step() {
@@ -15,10 +53,17 @@ export default class PopupMenuGameObjectRender extends GameObjectRender {
   }
 
   draw(independentCanvas) {
-	throw "Method 'draw' is not implemented";
+    if ((this.#popupMenuGameObject.visible) && (!this.#dialogEl.open)) {
+      this.#dialogEl.showModal();
+    }
   }
 
   get popupMenuGameObject() {
     return this.#popupMenuGameObject;
+  }
+  
+  release() {
+    super.release();
+    this.#dialogEl.remove();
   }
 }
