f6fc592eca4559a063b4f7dca962d2c54f3ba9b6
Fedor Urvanov <fedor@urvanov.ru>
2023-12-20 18:12:12 +0300

    ru messages to cache

--- src/sw.js
+++ src/sw.js
@@ -5,12 +5,13 @@ self.addEventListener('install', (e) => {
       '/projects/games/virtualpets/index.html',
       '/projects/games/virtualpets/main.js',
       '/projects/games/virtualpets/data/locales/en/messages.json',
+      '/projects/games/virtualpets/data/locales/ru/messages.json'
     ])),
   );
 });
  
 self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
+  console.log('ServiceWorker is fetching %s', e.request.url);
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request)),
   );
